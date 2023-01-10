0fe63f00e8b9f4fa519538fd8baebf5956dc176f
Sebastian Peschl <sebastian.peschl@xidv.de>
2023-01-10 12:56:41 +0100

    Lösung Teilaufgabe A erstellt + farbe ergänzt + var ergänzt

--- script/script.js
+++ script/script.js
@@ -1,5 +1,25 @@
 $(() => {
     console.log('Document ready for action ...');
 
-    
+    setNameOut();
+
+    $('#name').on('keyup', e => {
+        $('#instantReprint').val($(e.target).val());
+    });
+
+    $('#name').on('focusout', e => {
+        if($(e.target).val() === '') {
+            $('#submit').trigger('click');
+        }
+    });
+
+    $('#submit').on('click', e => {
+        setNameOut($('#name').val());
+    });
+
 });
+
+
+function setNameOut(name) {
+    name ? $('#nameOut').text(name) : $('#nameOut').text($('#name').attr('placeholder'));
+}
